9e574f0c29d6e700208177fedce6bc13655106f9
mingking2 <mingee641@gmail.com>
2025-04-15 19:52:59 +0900

    init commit

---
+++ src/main/java/com/allclearwas/AllclearWasApplication.java
@@ -0,0 +1,13 @@
+package com.allclearwas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllclearWasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllclearWasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/allclearwas/AllclearWasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allclearwas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllclearWasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
