0ca6694b6c5667116d7c776b2af3a7214cfa80ed
mingking2 <mingee641@gmail.com>
2025-05-04 19:49:54 +0900

    feat: CourseTime 엔티티 추가

---
+++ src/main/java/com/allclearwas/domains/course/domain/CourseTime.java
@@ -0,0 +1,40 @@
+package com.allclearwas.domains.course.domain;
+
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CourseTime {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "course_time_id")
+	private Long id;
+
+	@Enumerated(EnumType.STRING)
+	private DayOfWeek dayOfWeek;
+
+	private LocalTime startTime;
+	private LocalTime endTime;
+
+	@Builder
+	public CourseTime(DayOfWeek dayOfWeek, LocalTime startTime, LocalTime endTime) {
+		this.dayOfWeek = dayOfWeek;
+		this.startTime = startTime;
+		this.endTime = endTime;
+	}
+}
