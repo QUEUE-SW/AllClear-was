c77ff2bf78f27ffc1185958369eff2b260106e90
mingking2 <mingee641@gmail.com>
2025-05-04 19:32:40 +0900

    feat: Course 엔티티 추가

---
+++ src/main/java/com/allclearwas/domains/course/domain/Course.java
@@ -0,0 +1,52 @@
+package com.allclearwas.domains.course.domain;
+
+import com.allclearwas.domains.course.type.Category;
+import com.allclearwas.domains.course.type.Location;
+import com.allclearwas.domains.course.type.Professor;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Course {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "course_id")
+	private Long id;
+
+	private String name;
+
+	@Enumerated(EnumType.STRING)
+	private Professor professor;
+
+	@Enumerated(EnumType.STRING)
+	private Location location;
+
+	private int credit;
+	private int capacity;
+
+	@Enumerated(EnumType.STRING)
+	private Category category;
+
+	@Builder
+	public Course(String name, Professor professor, Location location, int credit, int capacity, Category category) {
+		this.name = name;
+		this.professor = professor;
+		this.location = location;
+		this.credit = credit;
+		this.capacity = capacity;
+		this.category = category;
+	}
+}
