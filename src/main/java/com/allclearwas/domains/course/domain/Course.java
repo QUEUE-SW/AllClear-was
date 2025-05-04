22aa0d7c08285fb2bc60f767de4a0f0aa6373d1c
mingking2 <mingee641@gmail.com>
2025-05-04 19:46:01 +0900

    feat: CourseInfo <-> Course 일대다 연관관계 설정

--- src/main/java/com/allclearwas/domains/course/domain/Course.java
+++ src/main/java/com/allclearwas/domains/course/domain/Course.java
@@ -8,9 +8,12 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -39,6 +42,10 @@ public class Course {
 
 	@Enumerated(EnumType.STRING)
 	private Category category;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "courseInfo")
+	private CourseInfo courseInfo;
 
 	@Builder
 	public Course(String name, Professor professor, Location location, int credit, int capacity, Category category) {
