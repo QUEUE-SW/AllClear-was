3032b4de1e026c94d796b13925fbc3e9b521ba78
mingking2 <mingee641@gmail.com>
2025-05-12 13:11:40 +0900

    feat: Spring Security 설정에 JWT 인증 필터 및 인증 실패 처리 로직 적용

---
+++ src/main/java/com/allclearwas/config/security/WebSecurityConfig.java
@@ -0,0 +1,38 @@
+package com.allclearwas.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.SecurityFilterChain;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+
+	private final FilterRegisterConfig filterRegisterConfig;
+	private final AuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.securityMatcher("/api/**")
+			.csrf(AbstractHttpConfigurer::disable)
+			.cors(Customizer.withDefaults())
+			.authorizeHttpRequests(request -> {
+				request.requestMatchers("/api-docs/**", "/v3/api-docs/**", "/swagger-ui/**", "/swagger").permitAll();
+				request.requestMatchers("/api/v1/auth/**")
+					.anonymous();
+				request.requestMatchers("/api/v1/sign-out").authenticated();
+				request.anyRequest().permitAll();
+			})
+			.with(filterRegisterConfig, Customizer.withDefaults())
+			.exceptionHandling(handler -> {
+				handler.authenticationEntryPoint(jwtAuthenticationEntryPoint);
+			});
+		return http.build();
+	}
+}
