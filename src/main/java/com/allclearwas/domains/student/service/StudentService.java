43f4db02c3ae53627aa3599075b85872fd373750
MinjiSeo16 <mjlove6630@naver.com>
2025-05-13 17:51:48 +0900

    feat: 학생 기본정보 조회 로직 서비스 구현

--- src/main/java/com/allclearwas/domains/student/service/StudentService.java
+++ src/main/java/com/allclearwas/domains/student/service/StudentService.java
@@ -4,7 +4,10 @@ import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
+import com.allclearwas.common.exception.GlobalException;
+import com.allclearwas.common.exception.student.StudentErrorCode;
 import com.allclearwas.domains.student.domain.Student;
+import com.allclearwas.domains.student.dto.response.StudentInfoResponse;
 import com.allclearwas.domains.student.repository.StudentRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -19,5 +22,16 @@ public class StudentService {
 
 	public Optional<Student> readStudent(Long studentId) {
 		return studentRepository.findById(studentId);
+	}
+
+	public StudentInfoResponse getStudentInfo(Long studentId) {
+		Student student = studentRepository.findById(studentId)
+			.orElseThrow(() -> new GlobalException(StudentErrorCode.STUDENT_NOT_FOUND));
+
+		return new StudentInfoResponse(
+			student.getName(),
+			student.getIdentifier(),
+			student.getStudentPolicy().getSemester()
+		);
 	}
 }
