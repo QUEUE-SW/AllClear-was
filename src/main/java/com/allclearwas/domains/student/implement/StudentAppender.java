568756406935f1940ea6009b2164d234763aba1d
mingking2 <mingee641@gmail.com>
2025-05-13 16:27:17 +0900

    feat: StudentAppender 추가

---
+++ src/main/java/com/allclearwas/domains/student/implement/StudentAppender.java
@@ -0,0 +1,17 @@
+package com.allclearwas.domains.student.implement;
+
+import com.allclearwas.common.annotation.Implementation;
+import com.allclearwas.domains.student.domain.Student;
+import com.allclearwas.domains.student.repository.StudentRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Implementation
+@RequiredArgsConstructor
+public class StudentAppender {
+	private final StudentRepository studentRepository;
+
+	public void append(Student student) {
+		studentRepository.save(student);
+	}
+}
