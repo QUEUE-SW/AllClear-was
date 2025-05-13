ec4514c3f1816230897dbabf38f1950e0eb23132
MinjiSeo16 <mjlove6630@naver.com>
2025-05-13 17:53:41 +0900

    docs: Swagger에 학생 기본정보 조회 API 명세 추가

--- src/main/java/com/allclearwas/domains/student/controller/StudentController.java
+++ src/main/java/com/allclearwas/domains/student/controller/StudentController.java
@@ -10,6 +10,10 @@ import com.allclearwas.common.security.authentication.SecurityUserDetails;
 import com.allclearwas.domains.student.dto.response.StudentInfoResponse;
 import com.allclearwas.domains.student.service.StudentService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -18,6 +22,17 @@ import lombok.RequiredArgsConstructor;
 public class StudentController {
 
 	private final StudentService studentService;
+
+	@Operation(
+		summary = "학생 기본정보 조회",
+		description = "로그인한 학생의 이름, 학번, 학기를 조회합니다.",
+		security = {@SecurityRequirement(name = "JWT")}
+	)
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "2000", description = "학생 정보 조회 성공"),
+		@ApiResponse(responseCode = "404", description = "해당 학생이 존재하지 않음"),
+		@ApiResponse(responseCode = "403", description = "JWT 인증 실패")
+	})
 
 	@GetMapping("/me")
 	public SuccessResponse<StudentInfoResponse> getStudentInfo(
