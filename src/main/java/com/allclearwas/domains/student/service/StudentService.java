ec02ba4cbe276404639c44ea7be79945fe94adc6
MinjiSeo16 <mjlove6630@naver.com>
2025-05-15 18:39:04 +0900

    refactor: 학생 서비스 로직 변경(implement적용, 공통 예외처리 변경)

---
+++ src/main/java/com/allclearwas/domains/student/service/StudentService.java
@@ -0,0 +1,31 @@
+package com.allclearwas.domains.student.service;
+
+import org.springframework.stereotype.Service;
+
+import com.allclearwas.common.exception.student.StudentErrorCode;
+import com.allclearwas.common.exception.student.StudentException;
+import com.allclearwas.domains.student.domain.Student;
+import com.allclearwas.domains.student.domain.StudentPolicy;
+import com.allclearwas.domains.student.dto.response.StudentProfileRes;
+import com.allclearwas.domains.student.implement.StudentPolicyReader;
+import com.allclearwas.domains.student.implement.StudentReader;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class StudentService {
+
+	private final StudentReader studentReader;
+	private final StudentPolicyReader studentPolicyReader;
+
+	public StudentProfileRes getStudentInfo(Long studentId) {
+		Student student = studentReader.read(studentId)
+			.orElseThrow(() -> new StudentException(StudentErrorCode.STUDENT_NOT_FOUND));
+
+		StudentPolicy studentPolicy = studentPolicyReader.read(studentId)
+			.orElseThrow(() -> new StudentException(StudentErrorCode.STUDENT_POLICY_NOT_FOUND));
+
+		return StudentProfileRes.from(student, studentPolicy);
+	}
+}
