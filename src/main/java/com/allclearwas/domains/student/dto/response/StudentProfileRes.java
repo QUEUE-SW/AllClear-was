9be835bd45704c816da62c3561c54599a0dac5f6
MinjiSeo16 <mjlove6630@naver.com>
2025-05-15 18:37:21 +0900

    refactor: StudentProfileRes 이름 변경

--- src/main/java/com/allclearwas/domains/student/dto/response/StudentProfileRes.java
+++ src/main/java/com/allclearwas/domains/student/dto/response/StudentProfileRes.java
@@ -1,10 +1,12 @@
 package com.allclearwas.domains.student.dto.response;
 
+import com.allclearwas.domains.student.domain.Student;
+import com.allclearwas.domains.student.domain.StudentPolicy;
 import com.allclearwas.domains.student.type.Semester;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
-public record StudentInfoResponse(
+public record StudentProfileRes(
 	@Schema(description = "학생 이름", example = "박지원")
 	String name,
 	@Schema(description = "학번 (고유 ID)", example = "22012077")
@@ -12,4 +14,11 @@ public record StudentInfoResponse(
 	@Schema(description = "현재 학기", example = "1학기")
 	Semester semester
 ) {
+	public static StudentProfileRes from(Student student, StudentPolicy studentPolicy) {
+		return new StudentProfileRes(
+			student.getName(),
+			student.getIdentifier(),
+			studentPolicy.getSemester()
+		);
+	}
 }
