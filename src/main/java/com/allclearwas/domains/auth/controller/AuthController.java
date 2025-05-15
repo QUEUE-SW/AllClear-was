ef2f365e063db423900bdb8110d31ffa73542944
mingking2 <mingee641@gmail.com>
2025-05-15 10:31:57 +0900

    refactor: 스웨거 및 인가상태, 검증 적용

--- src/main/java/com/allclearwas/domains/auth/controller/AuthController.java
+++ src/main/java/com/allclearwas/domains/auth/controller/AuthController.java
@@ -1,27 +1,32 @@
 package com.allclearwas.domains.auth.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.allclearwas.common.response.SuccessResponse;
+import com.allclearwas.domains.auth.api.AuthApi;
 import com.allclearwas.domains.auth.dto.request.SignupReq;
 import com.allclearwas.domains.auth.dto.response.SignupRes;
 import com.allclearwas.domains.auth.service.AuthService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
-public class AuthController {
+public class AuthController implements AuthApi {
 
 	private final AuthService authService;
 
+	@Override
+	@PreAuthorize("isAnonymous()")
 	@PostMapping("/sign-up")
-	public ResponseEntity<?> signUp(@RequestBody SignupReq signupReq) {
+	public ResponseEntity<?> signUp(@Valid @RequestBody SignupReq signupReq) {
 		SignupRes signupRes = authService.signup(signupReq);
 		return ResponseEntity.ok(SuccessResponse.of(signupRes));
 	}
