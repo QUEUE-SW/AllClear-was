5a1a2d285138aea01f5fdb179fde108439062b55
mingking2 <mingee641@gmail.com>
2025-05-15 10:29:21 +0900

    feat: 학번 중복 검증 로직 추가 및 적용

--- src/main/java/com/allclearwas/domains/auth/service/AuthService.java
+++ src/main/java/com/allclearwas/domains/auth/service/AuthService.java
@@ -7,6 +7,7 @@ import com.allclearwas.domains.auth.dto.request.SignupReq;
 import com.allclearwas.domains.auth.dto.response.SignupRes;
 import com.allclearwas.domains.student.domain.Student;
 import com.allclearwas.domains.student.implement.StudentAppender;
+import com.allclearwas.domains.student.implement.StudentValidator;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,8 +18,11 @@ import lombok.extern.slf4j.Slf4j;
 public class AuthService {
 	private final PasswordEncoder passwordEncoder;
 	private final StudentAppender studentAppender;
+	private final StudentValidator studentValidator;
 
 	public SignupRes signup(SignupReq signupReq) {
+		studentValidator.checkDuplicateStudent(signupReq.identifier());
+
 		Student student = signupReq.toEntity(passwordEncoder);
 		studentAppender.append(student);
 
