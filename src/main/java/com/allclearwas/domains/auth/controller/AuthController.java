0ebb22e1fd04652fc95214e1ca926d15005c0e3e
mingking2 <mingee641@gmail.com>
2025-05-17 14:09:00 +0900

    feat: 사용자 로그인 로직 추가

--- src/main/java/com/allclearwas/domains/auth/controller/AuthController.java
+++ src/main/java/com/allclearwas/domains/auth/controller/AuthController.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.allclearwas.common.response.SuccessResponse;
 import com.allclearwas.domains.auth.api.AuthApi;
+import com.allclearwas.domains.auth.dto.request.SignInReq;
 import com.allclearwas.domains.auth.dto.request.SignupReq;
+import com.allclearwas.domains.auth.dto.response.SignInRes;
 import com.allclearwas.domains.auth.dto.response.SignupRes;
 import com.allclearwas.domains.auth.service.AuthService;
 
@@ -29,5 +31,13 @@ public class AuthController implements AuthApi {
 	public ResponseEntity<?> signUp(@Valid @RequestBody SignupReq signupReq) {
 		SignupRes signupRes = authService.signup(signupReq);
 		return ResponseEntity.ok(SuccessResponse.of(signupRes));
+	}
+
+	@Override
+	@PreAuthorize("isAnonymous()")
+	@PostMapping("/sign-in")
+	public ResponseEntity<?> signIn(@Valid @RequestBody SignInReq signInReq) {
+		SignInRes signInRes = authService.signIn(signInReq);
+		return ResponseEntity.ok(SuccessResponse.of(signInRes));
 	}
 }
--- src/main/java/com/allclearwas/domains/auth/service/AuthService.java
+++ src/main/java/com/allclearwas/domains/auth/service/AuthService.java
@@ -3,10 +3,18 @@ package com.allclearwas.domains.auth.service;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.allclearwas.common.exception.auth.AuthErrorCode;
+import com.allclearwas.common.exception.auth.AuthException;
+import com.allclearwas.common.exception.student.StudentErrorCode;
+import com.allclearwas.common.exception.student.StudentException;
+import com.allclearwas.domains.auth.dto.request.SignInReq;
 import com.allclearwas.domains.auth.dto.request.SignupReq;
+import com.allclearwas.domains.auth.dto.response.SignInRes;
 import com.allclearwas.domains.auth.dto.response.SignupRes;
+import com.allclearwas.domains.auth.implement.TokenGenerator;
 import com.allclearwas.domains.student.domain.Student;
 import com.allclearwas.domains.student.implement.StudentAppender;
+import com.allclearwas.domains.student.implement.StudentReader;
 import com.allclearwas.domains.student.implement.StudentValidator;
 
 import lombok.RequiredArgsConstructor;
@@ -18,7 +26,9 @@ import lombok.extern.slf4j.Slf4j;
 public class AuthService {
 	private final PasswordEncoder passwordEncoder;
 	private final StudentAppender studentAppender;
+	private final StudentReader studentReader;
 	private final StudentValidator studentValidator;
+	private final TokenGenerator tokenGenerator;
 
 	public SignupRes signup(SignupReq signupReq) {
 		studentValidator.checkDuplicateStudent(signupReq.identifier());
@@ -28,4 +38,18 @@ public class AuthService {
 
 		return SignupRes.from(student.getId());
 	}
+
+	public SignInRes signIn(SignInReq signInReq) {
+		log.debug("SignInReq: {}", signInReq);
+		Student student = studentReader.readByIdentifier(signInReq.identifier())
+			.orElseThrow(() -> new StudentException(StudentErrorCode.STUDENT_NOT_FOUND));
+
+		if (!passwordEncoder.matches(signInReq.password(), student.getPassword())) {
+			throw new AuthException(AuthErrorCode.PASSWORD_MISMATCH);
+		}
+
+		String token = tokenGenerator.generateToken(student.getId(), student.getIdentifier());
+		return SignInRes.of(student.getId(), token);
+	}
+
 }
