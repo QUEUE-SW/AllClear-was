29664b86b5252e4d075ae246633b2d72e12606c4
MinjiSeo16 <mjlove6630@naver.com>
2025-05-18 11:41:50 +0900

    feat: CourseController 메서드 추가

--- src/main/java/com/allclearwas/domains/course/controller/CourseController.java
+++ src/main/java/com/allclearwas/domains/course/controller/CourseController.java
@@ -3,12 +3,15 @@ package com.allclearwas.domains.course.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.allclearwas.common.response.SuccessResponse;
+import com.allclearwas.common.security.authentication.SecurityUserDetails;
 import com.allclearwas.domains.course.dto.response.CourseListRes;
+import com.allclearwas.domains.course.dto.response.MyCourseListRes;
 import com.allclearwas.domains.course.service.CourseService;
 
 import lombok.RequiredArgsConstructor;
@@ -23,6 +26,13 @@ public class CourseController {
 	@GetMapping
 	public ResponseEntity<?> getCourseList() {
 		List<CourseListRes> response = courseService.getCourseList();
+		return ResponseEntity.ok(SuccessResponse.of(response));
+	}
+
+	@GetMapping("/me")
+	public ResponseEntity<?> getMyCourses(@AuthenticationPrincipal SecurityUserDetails userDetails) {
+		Long studentId = userDetails.getStudentId();
+		List<MyCourseListRes> response = courseService.getMyCourses(studentId);
 		return ResponseEntity.ok(SuccessResponse.of(response));
 	}
 }
