db9b1a3e103413b8989a5a1a3205f88e70081a52
MinjiSeo16 <mjlove6630@naver.com>
2025-05-18 11:51:16 +0900

    docs: security 부분 삭제

--- src/main/java/com/allclearwas/domains/course/api/CourseListApi.java
+++ src/main/java/com/allclearwas/domains/course/api/CourseListApi.java
@@ -7,7 +7,6 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @Tag(name = "모든 강의 조회 API")
@@ -15,8 +14,7 @@ public interface CourseListApi {
 
 	@Operation(
 		summary = "모든 강의정보 조회",
-		description = "개설된 모든 강의 목록을 조회한다.",
-		security = {@SecurityRequirement(name = "JWT")}
+		description = "개설된 모든 강의 목록을 조회한다."
 	)
 	@ApiResponses(value = {
 		@ApiResponse(responseCode = "2000", description = "강의 목록 조회 성공",
