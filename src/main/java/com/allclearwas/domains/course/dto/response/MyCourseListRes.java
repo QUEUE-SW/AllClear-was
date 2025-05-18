806c9c7730a8c1c68e3762aaa8aff26bd3305549
MinjiSeo16 <mjlove6630@naver.com>
2025-05-18 11:38:51 +0900

    feat: MyCourseListRes 추가

---
+++ src/main/java/com/allclearwas/domains/course/dto/response/MyCourseListRes.java
@@ -0,0 +1,33 @@
+package com.allclearwas.domains.course.dto.response;
+
+import com.allclearwas.domains.course.domain.Course;
+import com.allclearwas.domains.course.type.Location;
+import com.allclearwas.domains.course.type.Professor;
+
+public record MyCourseListRes(
+	Long enrollmentId,
+	Long courseId,
+	String courseCode,
+	String name,
+	Professor professor,
+	Location location,
+	int capacity,
+	int credit,
+	String time1,
+	String time2
+) {
+	public static MyCourseListRes of(Long enrollmentId, Course course, String time1, String time2) {
+		return new MyCourseListRes(
+			enrollmentId,
+			course.getId(),
+			course.getCourseCode(),
+			course.getName(),
+			course.getProfessor(),
+			course.getLocation(),
+			course.getCapacity(),
+			course.getCredit(),
+			time1,
+			time2
+		);
+	}
+}
