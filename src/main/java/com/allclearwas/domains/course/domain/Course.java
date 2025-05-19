e40320e49ffb8aac14b2488533efd8f4a872ba22
MinjiSeo16 <mjlove6630@naver.com>
2025-05-20 00:58:27 +0900

    feat: Course -> CourseTime 양방향 연관관계 매핑 추가

--- src/main/java/com/allclearwas/domains/course/domain/Course.java
+++ src/main/java/com/allclearwas/domains/course/domain/Course.java
@@ -1,5 +1,8 @@
 package com.allclearwas.domains.course.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.allclearwas.domains.course.type.Category;
 import com.allclearwas.domains.course.type.Location;
 import com.allclearwas.domains.course.type.Professor;
@@ -14,6 +17,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -47,6 +51,9 @@ public class Course {
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "courseInfo_id")
 	private CourseInfo courseInfo;
+
+	@OneToMany(mappedBy = "course", fetch = FetchType.LAZY)
+	private List<CourseTime> courseTimes = new ArrayList<>();
 
 	@Builder
 	public Course(String name, Professor professor, Location location, int credit, int capacity, Category category) {
