26760a76cca847898e36b9fbe550ad7185196a4d
mingking2 <mingee641@gmail.com>
2025-05-21 09:45:39 +0900

    test: Testcontainers 기반 MySQL 테스트 컨테이너 설정 추가

--- src/test/java/com/allclearwas/AllclearWasApplicationTests.java
+++ src/test/java/com/allclearwas/AllclearWasApplicationTests.java
@@ -2,9 +2,30 @@ package com.allclearwas;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
+@Testcontainers
 @SpringBootTest
 class AllclearWasApplicationTests {
+
+	private static final String MYSQL_CONTAINER_IMAGE = "mysql:8.0.35";
+	private static final MySQLContainer MYSQL_CONTAINER;
+
+	static {
+		MYSQL_CONTAINER = new MySQLContainer(DockerImageName.parse(MYSQL_CONTAINER_IMAGE));
+		MYSQL_CONTAINER.start();
+	}
+
+	@DynamicPropertySource
+	public static void setProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url", MYSQL_CONTAINER::getJdbcUrl);
+		registry.add("spring.datasource.username", MYSQL_CONTAINER::getUsername);
+		registry.add("spring.datasource.password", MYSQL_CONTAINER::getPassword);
+	}
 
 	@Test
 	void contextLoads() {
