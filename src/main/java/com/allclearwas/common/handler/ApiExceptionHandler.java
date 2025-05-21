58ea37958e9ae42f2a7ad786c55fe8e3ccfd31ff
mingking2 <mingee641@gmail.com>
2025-05-21 20:23:42 +0900

    remove: 미사용 import 제거

--- src/main/java/com/allclearwas/common/handler/ApiExceptionHandler.java
+++ src/main/java/com/allclearwas/common/handler/ApiExceptionHandler.java
@@ -1,8 +1,5 @@
 package com.allclearwas.common.handler;
 
-import java.nio.file.AccessDeniedException;
-
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
