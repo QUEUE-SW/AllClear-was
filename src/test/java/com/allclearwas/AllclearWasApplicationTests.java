ae08e6cb82946fbaaa0479fd874c7df15cb0800c
King's meow <mingee641@gmail.com>
2025-05-21 13:09:39 +0900

    [QUEUE-86] Github 자동화 파이프라인 구축 (#13)
    * chore: ci 워크플로우 작성
    
    * chore: Prod/Dev용 Dockefile 추가
    
    * chore: Prod/Dev용 배포 워크플로우 추가
    
    * chore: CI 테스트용 application-test.yml 파일 생성 로직 추가
    
    * chore: test 환경 구축을 위한 testcontainers 의존성 추가
    
    * chore: ci test 기존으로 수정
    
    * test: Testcontainers 기반 MySQL 테스트 컨테이너 설정 추가
    
    * test: 공통 MySQL Testcontainer 설정 추상 클래스 분리
    
    * close: #QUEUE-86

--- src/test/java/com/allclearwas/AllclearWasApplicationTests.java
+++ src/test/java/com/allclearwas/AllclearWasApplicationTests.java
@@ -2,9 +2,14 @@ package com.allclearwas;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
-class AllclearWasApplicationTests {
+class AllclearWasApplicationTests extends TestDatabaseConfig {
 
 	@Test
 	void contextLoads() {
