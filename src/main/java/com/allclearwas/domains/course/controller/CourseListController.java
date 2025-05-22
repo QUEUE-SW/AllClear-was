edb2f2a0842c2b19352b9b7c1aea438370cfb570
MinjiSeo16 <mjlove6630@naver.com>
2025-05-22 20:09:31 +0900

    refactor: CourseListController 수정

--- src/main/java/com/allclearwas/domains/course/controller/CourseListController.java
+++ src/main/java/com/allclearwas/domains/course/controller/CourseListController.java
@@ -5,11 +5,13 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.allclearwas.common.response.SuccessResponse;
 import com.allclearwas.domains.course.api.CourseListApi;
+import com.allclearwas.domains.course.dto.request.CourseFilterRequest;
 import com.allclearwas.domains.course.dto.response.CourseListRes;
 import com.allclearwas.domains.course.service.CourseService;
 
@@ -23,9 +25,9 @@ public class CourseListController implements CourseListApi {
 	private final CourseService courseService;
 
 	@PreAuthorize("isAuthenticated()")
-	@GetMapping
-	public ResponseEntity<?> getCourseList() {
-		List<CourseListRes> response = courseService.getCourseList();
+	@GetMapping("/filters")
+	public ResponseEntity<?> getfilterCourses(@ModelAttribute CourseFilterRequest request) {
+		List<CourseListRes> response = courseService.getfilterCourses(request);
 		return ResponseEntity.ok(SuccessResponse.of(response));
 	}
 }
