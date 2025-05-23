de42e845baabd8055780b636cda297b76703c8b8
MinjiSeo16 <mjlove6630@naver.com>
2025-05-23 23:30:52 +0900

    refactor: QueryDslCourseRepositoryImpl 메서드 분리

--- src/main/java/com/allclearwas/domains/course/repository/impl/QueryDslCourseRepositoryImpl.java
+++ src/main/java/com/allclearwas/domains/course/repository/impl/QueryDslCourseRepositoryImpl.java
@@ -10,9 +10,12 @@ import com.allclearwas.domains.course.domain.QCourseInfo;
 import com.allclearwas.domains.course.domain.QCourseTime;
 import com.allclearwas.domains.course.dto.request.CourseFilterRequest;
 import com.allclearwas.domains.course.repository.QueryDslCourseRepository;
-import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Expression;
 import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.core.types.dsl.SimpleExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
@@ -23,30 +26,14 @@ import lombok.RequiredArgsConstructor;
 public class QueryDslCourseRepositoryImpl implements QueryDslCourseRepository {
 
 	private final JPAQueryFactory queryFactory;
-
 	private final QCourse course = QCourse.course;
 	private final QCourseInfo courseInfo = QCourseInfo.courseInfo;
-	private final QCourseTime time1 = new QCourseTime("time1");
-	private final QCourseTime time2 = new QCourseTime("time2");
 
 	@Override
 	public List<CourseListDao> findFilteredCourses(CourseFilterRequest request) {
 
-		BooleanBuilder builder = new BooleanBuilder();
-		builder.and(course.courseInfo.eq(courseInfo));
-
-		if (request.category() != null) {
-			builder.and(courseInfo.category.eq(request.category()));
-		}
-		if (request.grade() != null) {
-			builder.and(courseInfo.grade.eq(request.grade()));
-		}
-		if (request.department() != null) {
-			builder.and(courseInfo.department.eq(request.department()));
-		}
-		if (request.code() != null && !request.code().isBlank()) {
-			builder.and(course.courseCode.eq(request.code()));
-		}
+		QCourseTime time1 = new QCourseTime("time1");
+		QCourseTime time2 = new QCourseTime("time2");
 
 		return queryFactory
 			.select(Projections.constructor(CourseListDao.class,
@@ -57,37 +44,39 @@ public class QueryDslCourseRepositoryImpl implements QueryDslCourseRepository {
 				course.location,
 				course.capacity,
 				course.credit,
-
-				JPAExpressions.select(
-						Expressions.stringTemplate(
-							"concat({0}, ' ', {1}, '~', {2})",
-							time1.dayOfWeek.stringValue(),
-							time1.startTime.stringValue(),
-							time1.endTime.stringValue()
-						)
-					)
-					.from(time1)
-					.where(time1.course.eq(course))
-					.orderBy(time1.id.asc())
-					.limit(1),
-
-				JPAExpressions.select(
-						Expressions.stringTemplate(
-							"concat({0}, ' ', {1}, '~', {2})",
-							time2.dayOfWeek.stringValue(),
-							time2.startTime.stringValue(),
-							time2.endTime.stringValue()
-						)
-					)
-					.from(time2)
-					.where(time2.course.eq(course))
-					.orderBy(time2.id.asc())
-					.offset(1)
-					.limit(1)
+				selectTimeString(time1, course, 0),
+				selectTimeString(time2, course, 1)
 			))
 			.from(course)
 			.join(course.courseInfo, courseInfo)
-			.where(builder)
+			.where(equalsIfNotNull(courseInfo.category, request.category()),
+				equalsIfNotNull(courseInfo.grade, request.grade()),
+				equalsIfNotNull(courseInfo.department, request.department()),
+				equalsIfNotBlank(course.courseCode, request.code()))
 			.fetch();
+	}
+
+	private <T> BooleanExpression equalsIfNotNull(SimpleExpression<T> path, T value) {
+		return value != null ? path.eq(value) : null;
+	}
+
+	private BooleanExpression equalsIfNotBlank(StringExpression path, String value) {
+		return value != null && !value.isBlank() ? path.eq(value) : null;
+	}
+
+	private Expression<String> selectTimeString(QCourseTime time, QCourse course, int offset) {
+		return JPAExpressions.select(
+				Expressions.stringTemplate(
+					"concat({0}, ' ', {1}, '~', {2})",
+					time.dayOfWeek.stringValue(),
+					time.startTime.stringValue(),
+					time.endTime.stringValue()
+				)
+			)
+			.from(time)
+			.where(time.course.eq(course))
+			.orderBy(time.id.asc())
+			.offset(offset)
+			.limit(1);
 	}
 }
