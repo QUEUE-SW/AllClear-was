7fccd952125fe4bf45ceca2e9d4404cf88592a5b
MinjiSeo16 <mjlove6630@naver.com>
2025-05-23 22:03:59 +0900

    feat: CourseRepository에 QueryDSL 커스텀 리포지토리 연동

--- src/main/java/com/allclearwas/domains/course/repository/CourseRepository.java
+++ src/main/java/com/allclearwas/domains/course/repository/CourseRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.allclearwas.domains.course.domain.Course;
 
-public interface CourseRepository extends JpaRepository<Course, Long> {
+public interface CourseRepository extends JpaRepository<Course, Long>, QueryDslCourseRepository {
 }
 
