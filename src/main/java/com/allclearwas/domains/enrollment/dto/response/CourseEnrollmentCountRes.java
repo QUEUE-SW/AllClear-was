c73d433d8b29fb00d372a73da11f33317a7ea423
MinjiSeo16 <mjlove6630@naver.com>
2025-06-04 15:59:01 +0900

    chore: 응답 필드명 변경

--- src/main/java/com/allclearwas/domains/enrollment/dto/response/CourseEnrollmentCountRes.java
+++ src/main/java/com/allclearwas/domains/enrollment/dto/response/CourseEnrollmentCountRes.java
@@ -1,12 +1,7 @@
 package com.allclearwas.domains.enrollment.dto.response;
 
-import com.allclearwas.domains.enrollment.dao.CourseEnrollmentCountDao;
-
 public record CourseEnrollmentCountRes(
 	Long courseId,
-	Long current
+	int participant
 ) {
-	public static CourseEnrollmentCountRes of(CourseEnrollmentCountDao dao) {
-		return new CourseEnrollmentCountRes(dao.getCourseId(), dao.getCount());
-	}
 }
