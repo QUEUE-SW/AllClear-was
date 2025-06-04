a4dda53571eca8d9609f60c9529b6501869a6c5e
MinjiSeo16 <mjlove6630@naver.com>
2025-06-04 16:01:33 +0900

    feat: 해당 participant 반환하는 JPQL 추가

--- src/main/java/com/allclearwas/domains/course/repository/CourseRepository.java
+++ src/main/java/com/allclearwas/domains/course/repository/CourseRepository.java
@@ -1,9 +1,18 @@
 package com.allclearwas.domains.course.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.allclearwas.domains.course.domain.Course;
+import com.allclearwas.domains.enrollment.dto.response.CourseEnrollmentCountRes;
 
 public interface CourseRepository extends JpaRepository<Course, Long>, QueryDslCourseRepository {
+
+	@Query("SELECT new com.allclearwas.domains.enrollment.dto.response.CourseEnrollmentCountRes(c.id, c.participant) "
+		+ "FROM Course c WHERE c.id IN :courseIds")
+	List<CourseEnrollmentCountRes> findParticipantsByCourseIds(@Param("courseIds") List<Long> courseIds);
 }
 
