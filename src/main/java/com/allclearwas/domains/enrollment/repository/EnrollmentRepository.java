c00fbb052a59b1330be17c0f5378f6ff61aebbdc
MinjiSeo16 <mjlove6630@naver.com>
2025-06-04 16:02:45 +0900

    refactor: 패키지 변경

--- src/main/java/com/allclearwas/domains/enrollment/repository/EnrollmentRepository.java
+++ src/main/java/com/allclearwas/domains/enrollment/repository/EnrollmentRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.allclearwas.domains.enrollment.domain.Enrollment;
 
-public interface EnrollmentRepository extends JpaRepository<Enrollment, Long>, QueryDslEnrollmentRepository {
+public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
 }
