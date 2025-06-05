2a8c61cc068cdee9b70c696ab4a8b5d0fa3756c0
MinjiSeo16 <mjlove6630@naver.com>
2025-06-06 02:28:43 +0900

    feat: EnrollmentController 추가

---
+++ src/main/java/com/allclearwas/domains/enrollment/controller/EnrollmentController.java
@@ -0,0 +1,39 @@
+package com.allclearwas.domains.enrollment.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.allclearwas.common.response.SuccessResponse;
+import com.allclearwas.common.security.authentication.SecurityUserDetails;
+import com.allclearwas.domains.enrollment.api.EnrollmentApi;
+import com.allclearwas.domains.enrollment.dto.request.EnrollmentReq;
+import com.allclearwas.domains.enrollment.dto.response.EnrollmentRes;
+import com.allclearwas.domains.enrollment.service.EnrollmentService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/enrollments")
+public class EnrollmentController implements EnrollmentApi {
+
+	private final EnrollmentService enrollmentService;
+
+	@PreAuthorize("isAuthenticated()")
+	@PostMapping
+	public ResponseEntity<?> enrollCourse(@RequestBody EnrollmentReq request,
+		@AuthenticationPrincipal SecurityUserDetails userDetails) {
+		log.debug("Requested courseId: {}", request.courseId());
+		Long studentId = userDetails.getStudentId();
+		EnrollmentRes response = enrollmentService.enrollCourse(request.courseId(), studentId);
+		log.debug("Enrollment result: {}", response);
+		return ResponseEntity.ok(SuccessResponse.of(response));
+	}
+}
