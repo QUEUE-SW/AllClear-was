f9ddf0e0b2a0b857831f260fe30777e88d7a7a14
MinjiSeo16 <151079127+MinjiSeo16@users.noreply.github.com>
2025-06-26 16:17:06 +0900

    [QUEUE-179] 비밀번호 암호화 적용 (#32)
    * refactor: 비밀번호 암호화 적용
    
    * feat: Component 주석처리

--- src/main/java/com/allclearwas/domains/student/devtool/StudentDataInitializer.java
+++ src/main/java/com/allclearwas/domains/student/devtool/StudentDataInitializer.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.allclearwas.domains.student.domain.Student;
 import com.allclearwas.domains.student.domain.StudentPolicy;
@@ -23,11 +24,14 @@ public class StudentDataInitializer implements CommandLineRunner {
 
 	private final StudentRepository studentRepository;
 	private final StudentPolicyRepository studentPolicyRepository;
+	private final PasswordEncoder passwordEncoder;
 	private static final String TESTNAME = "test";
+	private static final String PASSWORD = "test1234";
 
 	@Override
 	@Transactional
 	public void run(String... args) {
+		System.out.println("🔥 StudentDataInitializer 실행됨");
 		int total = 10_000;
 		int perMajor = 2_500;
 		Major[] majors = {Major.CSE, Major.ICE, Major.SC, Major.ALL};
@@ -42,17 +46,20 @@ public class StudentDataInitializer implements CommandLineRunner {
 
 			Student student = Student.builder()
 				.identifier(identifier)
-				.password("test1234")
+				.password(passwordEncoder.encode(PASSWORD))
 				.name(name)
 				.college(College.DIGITAL_CONVERGENCE)
 				.department(Department.COMPUTER_SCIENCE_AND_ENGINEERING)
 				.major(major)
+				.grade(4)
 				.build();
 
 			students.add(student);
+			System.out.println("✅ student 추가 완료" + i);
 		}
 
 		studentRepository.saveAll(students);
+		System.out.println("✅ student 저장 완료");
 
 		for (Student student : students) {
 			StudentPolicy policy = StudentPolicy.builder()
@@ -64,9 +71,11 @@ public class StudentDataInitializer implements CommandLineRunner {
 				.build();
 
 			policies.add(policy);
+			System.out.println("✅ student_policy 추가 완료" + student.getId());
 		}
 
 		studentPolicyRepository.saveAll(policies);
+		System.out.println("student_policy 저장 완료");
 	}
 }
 
