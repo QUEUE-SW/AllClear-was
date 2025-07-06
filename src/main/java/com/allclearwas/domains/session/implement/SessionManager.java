82a8da130cb04174922352636c11ad43f1fedccd
mingking2 <mingee641@gmail.com>
2025-07-06 14:41:35 +0900

    test: 200명으로 수정

--- src/main/java/com/allclearwas/domains/session/implement/SessionManager.java
+++ src/main/java/com/allclearwas/domains/session/implement/SessionManager.java
@@ -3,8 +3,6 @@ package com.allclearwas.domains.session.implement;
 import java.util.Map;
 
 import com.allclearwas.common.annotation.Implementation;
-import com.allclearwas.common.exception.auth.AuthErrorCode;
-import com.allclearwas.common.exception.auth.AuthException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -12,7 +10,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class SessionManager {
 
-	private final int MAX_CONCURRENT_USERS = 1;
+	private final int MAX_CONCURRENT_USERS = 200;
 	private final Map<Long, Long> activeUsers;
 
 	public void registerUser(Long studentId) {
