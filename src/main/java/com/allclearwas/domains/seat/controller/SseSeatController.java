9688b9ff87a5eca9f35828fc2c5f8c714e4b064d
MinjiSeo16 <mjlove6630@naver.com>
2025-08-09 13:15:06 +0900

    feat: HttpServletRequest 로그 추가

--- src/main/java/com/allclearwas/domains/seat/controller/SseSeatController.java
+++ src/main/java/com/allclearwas/domains/seat/controller/SseSeatController.java
@@ -13,6 +13,7 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import com.allclearwas.common.security.authentication.SecurityUserDetails;
 import com.allclearwas.domains.seat.service.SseSeatService;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -26,8 +27,12 @@ public class SseSeatController {
 
 	@GetMapping(value = "/subscribe", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
 	public SseEmitter subscribe(@RequestParam List<Long> courseIds,
-		@AuthenticationPrincipal SecurityUserDetails userDetails) {
+		@AuthenticationPrincipal SecurityUserDetails userDetails,
+		HttpServletRequest request) {
 		log.info("subscribe 시작");
+		log.info("요청 URL: {}", request.getRequestURL());
+		log.info("쿼리 스트링: {}", request.getQueryString());
+		log.info("클라이언트 IP: {}", request.getRemoteAddr());
 		Long studentId = userDetails.getStudentId();
 		return sseSeatService.subscribe(studentId, courseIds);
 	}
