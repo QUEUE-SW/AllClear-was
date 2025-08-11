94d8cc92eca42736f515edcbe92e50c72d0ba9ca
MinjiSeo16 <mjlove6630@naver.com>
2025-08-11 15:59:32 +0900

    feat: 서버 Heartbeat 로그 추가

--- src/main/java/com/allclearwas/domains/seat/implement/Heartbeat.java
+++ src/main/java/com/allclearwas/domains/seat/implement/Heartbeat.java
@@ -5,7 +5,9 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class Heartbeat {
@@ -17,6 +19,7 @@ public class Heartbeat {
 		sseEmitterManager.getEmitters().forEach((studentId, emitter) -> {
 			try {
 				emitter.send(SseEmitter.event().comment("ping"));
+				log.info("Heartbeat sse 알림 성공");
 			} catch (Exception e) {
 				sseEmitterManager.removeEmitter(studentId);
 			}
